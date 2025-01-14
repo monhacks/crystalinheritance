1c45c423719ac806539d5cc6d2ff42860d49c781
David-Gardner <dwg@serinuslabs.com>
2025-01-14 12:45:04 -0800

    cap cull gate and fixing plotscript

--- maps/CapitalCullGate.asm
+++ maps/CapitalCullGate.asm
@@ -1,4 +1,6 @@
-CapitalCullGate_MapScriptHeader: ;	def_scene_scripts
+CapitalCullGate_MapScriptHeader: 
+
+	def_scene_scripts
 
 
 	def_callbacks
@@ -17,7 +19,34 @@ CapitalCullGate_MapScriptHeader: ;	def_scene_scripts
 
 
 	def_object_events
-
+	object_event 3, 4, SPRITE_NOMAD_F, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, CapitalCullNomadFNPCScript, -1
 
 
 	object_const_def
+
+
+CapitalCullNomadFNPCScript:
+	jumptextfaceplayer CapitalCullGateNPCText
+
+CapitalCullGateNPCText:
+
+	text "Kensey speaks of"
+	line "progress, yet"
+	cont "I stand outside"
+	para "his moat, like"
+	line "a beggar."
+
+	para "I roamed free"
+	line "from shore to"
+	cont "shore. Now,"
+	para "I'm 'free' to"
+	line "make #balls"
+	cont "for them."
+
+	para "..."
+
+	para "The Sheltered"
+	line "Shores call to"
+	cont "me still."
+	done
+	
