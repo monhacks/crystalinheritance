fee61f7bc8de603e2c55c6bd6b852f2c6c24bbcc
David-Gardner <dwg@serinuslabs.com>
2025-03-27 06:02:17 -0700

    maps scripting

--- maps/SinjohCrossroads.asm
+++ maps/SinjohCrossroads.asm
@@ -6,20 +6,21 @@ SinjohCrossroads_MapScriptHeader:
 
 
 	def_warp_events
-	warp_event 0, 0, SINJOH_DORMS, 1;
-	warp_event 0, 0, SINJOH_STOCKROOM, 1;	
-	warp_event 0, 0, SINJOH_ELDERS_HOUSE, 1;
-	warp_event 0, 0, GROTTOED_GLACIER_1F, 1;
-	warp_event 0, 0, WESTERN_GLACIER_1F, 1;
+	warp_event 11, 19, SINJOH_DORMS, 1;
+	warp_event 19, 11, SINJOH_STOCKROOM, 1;	
+	warp_event 21, 17, SINJOH_ELDERS_HOUSE, 1;
+	warp_event 13, 5, GROTTOED_GLACIER_1F, 1;
+	warp_event 4, 7, WESTERN_GLACIER_1F, 1;
 
 	def_coord_events
 
 
 
 	def_bg_events
-	bg_event  0,  0, BGEVENT_JUMPTEXT, SinjohSign1Text
-	bg_event  0,  0, BGEVENT_JUMPTEXT, SinjohSign2Text
-
+	bg_event  18, 12, BGEVENT_JUMPTEXT, SinjohSign1TextStockroom
+	bg_event  20, 18, BGEVENT_JUMPTEXT, SinjohSign2TextElder
+	bg_event  20, 18, BGEVENT_JUMPTEXT, SinjohSign2TextDorms
+	bg_event  10, 20, BGEVENT_ITEM + FULL_HEAL, EVENT_SINJOH_HIDDEN_ITEM ; hidden item
 
 	def_object_events
 	object_event 0, 0, SPRITE_BRIGADER,		SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SinjohBrigader1Script, -1
@@ -28,6 +29,7 @@ SinjohCrossroads_MapScriptHeader:
 	object_event 0, 0, SPRITE_AROMA_LADY, 	SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, SinjohNPC1Text, EVENT_BEAT_KANNA
 	object_event 0, 0, SPRITE_HIKER, 		SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, SinjohNPC2Text, EVENT_BEAT_KANNA
 	object_event 0, 0, SPRITE_SAGE, 		SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, SinjohNPC3Text, EVENT_BEAT_KANNA
+	itemball_event 0, 0, FULL_RESTORE, 1, EVENT_SINJOH_ITEM
 	
 	object_const_def
 
